e0c95a993c4488145f4f03e5f05b66f09ae0e28f
Xavier Ordoquy <xordoquy@linovia.com>
2014-02-04 23:07:50 +0100

    Renamed the core into auth

--- bugzilla/settings.py
+++ bugzilla/settings.py
@@ -127,7 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
 
-    'bugzilla.core',
+    'bugzilla.auth',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
