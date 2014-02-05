2439cb7fd0a5bd4f3f059af4de2ddb6af5c2589d
Xavier Ordoquy <xordoquy@linovia.com>
2014-02-05 20:29:28 +0100

    Added packaging and test skeleton.

---
+++ setup.py
@@ -0,0 +1,78 @@
+"""
+setup
+~~~~~
+
+:copyright: (c) 2014 by ,
+see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+with open('requirements.txt') as f:
+    # TODO: skips comments
+    install_requires = f.read()
+
+
+tests_require = [
+    'pytest',
+    'pytest-cov>=1.4',
+    'pytest-django',
+]
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='bughipster',
+    version='0.1.0',
+    author='Xavier Ordoquy',
+    author_email='xordoquy@linovia.com',
+
+    url='https://github.com/linovia/bughipster',
+    description='',
+    long_description=open('README.md').read(),
+    license='BSD',
+
+    packages=[
+        'bughipster',
+    ],
+    package_dir={'bughipster': 'bughipster'},
+    zip_safe=False,
+
+    install_requires=install_requires,
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'bughipster = bughipster.runner:main',
+        ],
+    },
+
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+
+    cmdclass={'test': PyTest},
+    tests_require=tests_require,
+    extras_require={
+        'tests': tests_require,
+    },
+)
---
+++ tests/unit/conftest.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from bughipster import settings as base_settings
+
+
+def pytest_configure(config):
+    if not settings.configured:
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'NAME': ':memory:',
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'TEST_NAME': ':memory:',
+                },
+            },
+            INSTALLED_APPS=base_settings.INSTALLED_APPS,
+            ROOT_URLCONF=base_settings.ROOT_URLCONF,
+            DEBUG=False,
+            SITE_ID=1,
+            TEMPLATE_DEBUG=False,
+            ALLOWED_HOSTS=['*'],
+        )
