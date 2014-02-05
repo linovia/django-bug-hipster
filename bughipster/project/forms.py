b63a43f02ba87066c6a2e5f5f4c3a06287a9fc16
Xavier Ordoquy <xordoquy@linovia.com>
2014-02-06 00:04:46 +0100

    Reworked the form to include a crispy form helper and limit component and version to the project's.

--- bughipster/project/forms.py
+++ bughipster/project/forms.py
@@ -8,10 +8,13 @@ see AUTHORS for more details.
 """
 
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from . import models
 
 
 class Bug(forms.ModelForm):
+    version = forms.ModelChoiceField(queryset=models.Version.objects, empty_label=None)
     class Meta:
         model = models.Bug
         fields = (
@@ -20,3 +23,18 @@ class Bug(forms.ModelForm):
             # 'url', 'summary', 'estimate', 
             # 'blocks', 'description', 'dependson','hardware', 
         )
+
+    def __init__(self, product_id, *args, **kwargs):
+        self.product_id = product_id
+        super(Bug, self).__init__(*args, **kwargs)
+
+        # Limit the available components to the project's
+        qs = self.fields['component'].queryset
+        self.fields['component'].queryset = qs.filter(product_id=self.product_id)
+
+        # Limit the available versions to the project's
+        qs = self.fields['version'].queryset
+        self.fields['version'].queryset = qs.filter(product_id=self.product_id)
+
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
