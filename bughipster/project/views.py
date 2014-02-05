6c334891ea2bf6f8c6a9da65d89d0ad3cffd18f4
Xavier Ordoquy <xordoquy@linovia.com>
2014-02-06 00:05:21 +0100

    Invoke the refactored bug form.

--- bughipster/project/views.py
+++ bughipster/project/views.py
@@ -8,23 +8,47 @@ see AUTHORS for more details.
 """
 
 from django.views import generic
+from django import http
 
 from . import models
 from . import forms
 
 
-def bug_creation(request, *args, **kwargs):
-    if not request.GET.get('product', None):
-        return SelectProduct.as_view()(request, *args, **kwargs)
-    return CreateBug.as_view()(request, *args, **kwargs)
+class ProjectRestrictedMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        product_name = request.GET.get('product', None)
+        try:
+            self.product = models.Product.objects.get(name=product_name)
+        except models.Product.DoesNotExist:
+            return http.Http404()
+
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
 
 
-class CreateBug(generic.CreateView):
+class CreateBug(ProjectRestrictedMixin, generic.CreateView):
     template_name = 'new_bug.html'
     model = models.Bug
     form_class = forms.Bug
+
+    def get_form_kwargs(self):
+        kwargs = super(CreateBug, self).get_form_kwargs()
+        kwargs['product_id'] = self.product.id
+        return kwargs
 
 
 class SelectProduct(generic.ListView):
     template_name = 'product_selection.html'
     model = models.Product
+
+
+create_bug = CreateBug.as_view()
+select_product = SelectProduct.as_view()
+
+def bug_creation(request, *args, **kwargs):
+    if not request.GET.get('product', None):
+        return select_product(request, *args, **kwargs)
+    return create_bug(request, *args, **kwargs)
