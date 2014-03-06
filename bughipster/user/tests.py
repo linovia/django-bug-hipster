b88ef7e35ba278c51dc52ed6dff3bc040c0dc6a6
Xavier Ordoquy <xordoquy@linovia.com>
2014-03-06 07:36:22 +0100

    Added tests for the bugzilla's user model.

--- bughipster/user/tests.py
+++ bughipster/user/tests.py
@@ -6,11 +6,27 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from .hashers import BugzillaHasher
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class HasherTest(TestCase):
+    def test_encode(self):
+        encoded = "c56mZDsJ3qtdtvemXXw8v4P8mKTKetIDNtKneGM2q9Xek9mfWA0{SHA-256}"
+        hasher = BugzillaHasher()
+        result = hasher.encode('pipoti', "c56mZDsJ")
+        assert result == encoded, result
+
+    def test_verify(self):
+        encoded = "c56mZDsJ3qtdtvemXXw8v4P8mKTKetIDNtKneGM2q9Xek9mfWA0{SHA-256}"
+        hasher = BugzillaHasher()
+        assert hasher.verify('pipoti', encoded)
+        assert not hasher.verify('pipot', encoded)
+
+    def test_safe_summary(self):
+        encoded = "c56mZDsJ3qtdtvemXXw8v4P8mKTKetIDNtKneGM2q9Xek9mfWA0{SHA-256}"
+        hasher = BugzillaHasher()
+        assert hasher.safe_summary(encoded) == {
+            'algorithm': 'SHA-256',
+            'salt': 'c5******',
+            'hash': '3qtdtv*************************************'
+        }
