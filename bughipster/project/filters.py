5b819c35b531b46d78254265f931f4b076ead661
Xavier Ordoquy <xordoquy@linovia.com>
2014-03-14 21:36:35 +0100

    Moved the filters to the project app.

--- bughipster/project/filters.py
+++ bughipster/project/filters.py
@@ -1,22 +1,40 @@
 """
-bughipster.website.FilterSet
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+bughipster.project.filters
+~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 Filter bug list.
 """
 from __future__ import print_function, division
 from __future__ import absolute_import, unicode_literals
 
+from django import forms
 from django.utils.translation import ugettext_lazy as _
 import django_filters as filters
 
 from bughipster.project import models
 
 
+class StatusFilter(filters.ChoiceFilter):
+    def filter(self, qs, value):
+        lookup = self.lookup_type
+        if value in ([], (), {}, None, '', '__all__'):
+            return qs
+        if value == '__open__':
+            qs = qs.opened()
+        elif value == '__closed__':
+            qs = qs.closed()
+        else:
+            qs = qs.filter(**{'%s__%s' % (self.name, lookup): value})
+        if self.distinct:
+            qs = qs.distinct()
+        return qs
+
+
+
 class Bug(filters.FilterSet):
     product = filters.ModelMultipleChoiceFilter(
         queryset=models.Product.objects.all(),
-        to_field_name='name')
+        to_field_name='name', empty_label=_('All'))
     component = filters.ModelMultipleChoiceFilter(
         queryset=models.Component.objects.all(),
         to_field_name='name')
@@ -51,17 +69,18 @@ class Bug(filters.FilterSet):
 
 
 class SimpleQuery(filters.FilterSet):
-    bug_status = filters.ChoiceFilter(label=_('Status'), choices=(
+    bug_status = StatusFilter(label=_('Status'), choices=(
         ('__open__', _('Open')),
         ('__closed__', _('Closed')),
         ('__all__', _('All')),
-    ))
+    ), name='status')
     product = filters.ModelChoiceFilter(label=_('Product'),
         queryset=models.Product.objects.all(),
         to_field_name='name', empty_label=_('All'))
     content = filters.CharFilter(label=_('Words'))
+
     class Meta:
         model = models.Bug
         fields = [
-            'product',
+            'bug_status', 'product',
         ]
