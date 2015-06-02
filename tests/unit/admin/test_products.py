2bfb5cfe9283a2bdf4f13f03c2fe0a0258013459
Xavier Ordoquy <xordoquy@linovia.com>
2015-06-02 07:01:43 +0100

    Test against a nominal product change.

--- tests/unit/admin/test_products.py
+++ tests/unit/admin/test_products.py
@@ -27,12 +27,31 @@ def test_edit_product_that_does_not_exist(rf):
 
 
 def test_edit_product(rf):
-    models.Product.objects.create(
+    product = models.Product.objects.create(
         name='someproduct',
         description='Some product description.',
     )
-    request = rf.get('editproducts.cgi?action=edit&product=ekjzri')
+    new_description = 'Another description'
+    request = rf.post('editproducts.cgi?action=edit&product=someproduct', {
+        'product': 'someproduct',
+        'description': new_description,
+        # 'is_active': ,
+        'allows_unconfirmed': 'on',
+    })
+    expected_changes = {
+        'description': {
+            'new': 'Another description',
+            'old': 'Some product description.'
+        },
+        'isactive': {
+            'new': False,
+            'old': 1
+        }
+    }
     response = views.Products.as_view()(request)
-    assert 'message' in response.context_data
-    assert "Either the product 'ekjzri' does not exist or you don't have access to it." in response.context_data['message']
-    assert response.template_name == 'error.html'
+    assert response.status_code == 200
+    assert response.template_name == 'bz_admin/products/changes_summary.html'
+    assert response.context_data['changes'] == expected_changes
+    product = models.Product.objects.get(id=product.id)
+    assert product.description == new_description
+    assert not product.isactive
